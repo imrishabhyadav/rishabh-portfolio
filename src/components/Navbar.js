040ec39d00b627145a4365262bf70066db7a24ba
Rishabh Yadav <yaduvanshirishabh21@gmail.com>
2025-06-01 12:56:37 +0530

    next commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -89,7 +89,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/imrishabhyadav/Portfolio"
+                href="https://github.com/imrishabhyadav/rishabh-portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
